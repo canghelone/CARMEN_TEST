37d8681f6793e61d282f4455a923781ba359674b
canghelone <138444280+canghelone@users.noreply.github.com>
2023-07-03 11:16:43 +0200

    Update sql-injection.php

--- vulnerable-php-code-examples-master/vulnerable-php-code-examples-master/src/sql-injection.php
+++ vulnerable-php-code-examples-master/vulnerable-php-code-examples-master/src/sql-injection.php
@@ -10,6 +10,7 @@ if (NULL == $_GET['id']) $_GET['id'] = 1;
 
 $sql = 'SELECT * FROM employees WHERE employeeId = ' . $_GET['id'];
 
+
 foreach ($file_db->query($sql) as $row) {
     $employee = $row['LastName'] . " - " . $row['Email'] . "\n";
 
